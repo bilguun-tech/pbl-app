99278cebc22ef1ede0569672344dbcf5ed4cae19
soma <kuroguchi.soma.ee@tut.jp>
2024-02-15 02:30:56 +0900

    update

--- analysis_methods/just_plot.py
+++ analysis_methods/just_plot.py
@@ -2,10 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def just_plot(data, column_name):
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e95b791 (Update just_plot.py)
     # time = data.iloc[:, 0]
     # value = data[column_name]
     # 前処理
@@ -14,25 +10,11 @@ def just_plot(data, column_name):
     data = data[column_name].dropna()  # NaNの行を削除
 
     # 可視化
-<<<<<<< HEAD
     fig, ax = plt.subplots()
     # ax.plot(time, value)
     data.plot()
     plt.xlabel(x_label, fontname="MS Gothic")
     ax.ticklabel_format(style="plain", axis="y")  # 指数表記から普通の表記に変換
-=======
-    time = data.iloc[:, 0]
-    value = data[column_name]
-    fig, ax = plt.subplots()
-    ax.plot(time, value)
->>>>>>> 9e33400 (delete unnecessary datasets and improve arima and additive method source)
-=======
-    fig, ax = plt.subplots()
-    # ax.plot(time, value)
-    data.plot()
-    plt.xlabel(x_label, fontname="MS Gothic")
-    ax.ticklabel_format(style="plain", axis="y")  # 指数表記から普通の表記に変換
->>>>>>> e95b791 (Update just_plot.py)
     ax.set_title("Just Plot")
 
     return fig
